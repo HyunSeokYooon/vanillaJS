6996b24d775ffa13674e4470031d0b5cf649c2be
HyunSeokYoon <thdthd6245@gmail.com>
2022-07-04 23:49:56 +0900

    const, let, boolean까지 진행

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-alert("hi");
+//const & let
+const a = 5;
+const b = 2;
+let myName = "hyeonSeok";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log(myName);
+
+myName = "hyeonseokYoon";
+
+console.log(myName);
+
+
+//boolean
+const amIFat = null;
+let something;
+
+console.log(amIFat);
+console.log(something);
