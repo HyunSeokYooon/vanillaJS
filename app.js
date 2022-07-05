50008585c4d8bba205f457c4f3367d03b0f5cb05
HyunSeokYoon <thdthd6245@gmail.com>
2022-07-06 00:08:03 +0900

    create function and object

--- app.js
+++ app.js
@@ -19,3 +19,67 @@ let something;
 
 console.log(amIFat);
 console.log(something);
+
+//array
+const dayOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+
+//Get Item from Array
+console.log(dayOfWeek[0]);
+
+// Add one more day to the Array
+console.log(dayOfWeek);
+dayOfWeek.push("sun");
+console.log(dayOfWeek);
+
+
+//Create Object
+const player = {
+    name:"hyeonseok",
+    points: 10,
+    fat: true,
+};
+console.log(player);
+console.log(player.name);
+
+player.fat = false;
+console.log(player);
+
+player.lastName = "potate";
+console.log(player);
+
+player.points += 15;
+console.log(player);
+
+
+//Create function
+function sayHello(nameOfPerson, age){
+    console.log("Hello my name is " + nameOfPerson + "and I'm " + age);
+}
+
+sayHello("hyeonseok", 10);
+sayHello("sujin", 20);
+sayHello("somin", 29);
+sayHello("nico", 28);
+
+
+//Create Calc
+function plus(firstNumber, secondNumber){
+    console.log(firstNumber + secondNumber);
+}
+function divide(a, b){
+    console.log(a / b);
+}
+plus(8,60);
+divide(98,20);
+
+
+// Create object in function
+const player2 = {
+    name: "hyeonseok",
+    sayHello: function(otherPersonName){
+        console.log("hello " + otherPersonName + " nice to meet you");
+    },
+};
+
+player2.sayHello("hyeonseok");
+player2.sayHello("mejoung");
